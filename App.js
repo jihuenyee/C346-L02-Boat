da3fa7ae905efd773a50777b6e9bdaf9d744f437
jihuen <23026390@myrp.edu.sg>
2024-10-23 21:36:17 +0800

    L02 Boat

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { Text, ScrollView, StatusBar } from 'react-native';
 import Boat from './components/Boat.js';
 
 const App = () => {
@@ -7,24 +7,12 @@ const App = () => {
       <ScrollView>
             <StatusBar hidden={true}/>
             <Text>GetABoat - For Sale</Text>
-            <Boat icon_name="boat" name="Sea Ray 500 Sundancer"
-                  desc="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."
-                  pic={require("./img/sea_ray.jpg")}/>
-            <Boat icon_name="boat" name="Four Winns Horizon 180"
-                  desc="A sporty look and refined details truly set the Horizon 180 above all others."
-                  pic={require("./img/four_winns.jpg")}/>
-            <Boat icon_name="boat" name="Flipper 640 ST"
-                  desc="A modern take on the classic, traditional hardtop and perfect for a family picnic."
-                  pic={require("./img/flipper.jpg")}/>
-            <Boat icon_name="boat" name="Princess V48"
-                  desc="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior."
-                  pic={require("./img/princess.jpg")}/>
-            <Boat icon_name="boat" name="Bayliner 175 Bowrider"
-                  desc="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance."
-                  pic={require("./img/bayliner.jpg")}/>
-            <Boat icon_name="boat" name="Fairline Targa 47"
-                  desc="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit."
-                  pic={require("./img/fairline.jpg")}/>
+            <Boat icon_name="sailboat" name="Sea Ray 500 Sundancer" desc="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away." pic={require("./img/sea_ray.jpg")}/>
+            <Boat icon_name="sailboat" name="Four Winns Horizon 180" desc="A sporty look and refined details truly set the Horizon 180 above all others." pic={require("./img/four_winns.jpg")}/>
+            <Boat icon_name="sailboat" name="Flipper 640 ST" desc="A modern take on the classic, traditional hardtop and perfect for a family picnic." pic={require("./img/flipper.jpg")}/>
+            <Boat icon_name="sailboat" name="Princess V48" desc="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior." pic={require("./img/princess.jpg")}/>
+            <Boat icon_name="sailboat" name="Bayliner 175 Bowrider" desc="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance." pic={require("./img/bayliner.jpg")}/>
+            <Boat icon_name="sailboat" name="Fairline Targa 47" desc="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit." pic={require("./img/fairline.jpg")}/>
       </ScrollView>
   );
 };
--- components/Boat.js
+++ components/Boat.js
@@ -6,10 +6,13 @@ import Icon from "react-native-vector-icons/FontAwesome6";
 const Boat = ({ icon_name, name, desc, pic }) => {
     return (
         <View>
-            <Icon name={icon_name} size={20}>
-                <Text>{name} {desc}</Text>
+            <Icon name={icon_name} size={15}>
+                <Text>{name}</Text>
             </Icon>
+            <Text>{desc}</Text>
             <Image source={pic} style={{width:400, height:500}}/>
         </View>
     );
 };
+
+export default Boat;
